413ae967ccb1876d4a956c835d1577f9ddd8e372
Matthew Switlik <switlik@oakland.edu>
2016-12-01 17:13:44 -0500

    Use a plugin config setting for studentid

--- api.php
+++ api.php
@@ -8,6 +8,8 @@ require_once('./common.php');
 require_once("$CFG->libdir/gradelib.php");
 require_once("$CFG->dirroot/grade/querylib.php");
 
+require_once("$CFG->dirroot/user/profile/lib.php");
+
 function ak_get_validate_course_id($action) {
   global $AK_USER_ID;
 
@@ -19,18 +21,26 @@ function ak_get_validate_course_id($action) {
 }
 
 function ak_action_get_roster($action) {
+  global $CFG;
+  
   $course_id = ak_get_validate_course_id($action);
   $context = context_course::instance($course_id);
 
   $students = array();
   foreach (get_enrolled_users($context) as $id=>$student) {
+//    file_put_contents("/tmp/akindi.log", "akindi_student_id_field: ".$CFG->akindi_student_id_field."\n");
+    if ($CFG->akindi_student_id_field == "idnumber") {
+        $idnumber = $student->idnumber;
+    } else if ($CFG->akindi_student_id_field == "userid") {
+        $idnumber = $id;
+    } else {
+        // Use a custom profile field for the idnumber
+        profile_load_custom_fields($student);
+        $idnumber = $student->profile[$CFG->akindi_student_id_field];
 
-    // Replace the $student->idnumber with a custom profile field
-    profile_load_custom_fields($student);
-    $gid = $student->profile->gid;
-    $idnumber = preg_replace("/[^0-9]/", "", $gid);
-
-    //$idnumber = $student->idnumber;
+        // Remove any characters that are not 0-9.
+        $idnumber = preg_replace("/[^0-9]/", "", $idnumber);
+    }
     
     array_push($students, array(
       'student_id'=>$idnumber,
