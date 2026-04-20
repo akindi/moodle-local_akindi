79eaa16c3e2b18ca197455aa067369eaa54ead52
Andrew Hull <aghull@gmail.com>
2026-04-20 17:06:44 -0400

    keep min version back to v2

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
  
 $plugin->version   = 2026042000;
-$plugin->requires  = 2018120310;
+$plugin->requires  = 2014051200;
 $plugin->cron      = 0;
 $plugin->component = 'local_akindi';
 $plugin->maturity  = MATURITY_STABLE;
